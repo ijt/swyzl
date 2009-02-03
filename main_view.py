9c55467c7154210f8891603e1c8bd2ed0f0355a0
Issac Trotts <issac.trotts@gmail.com>
2009-02-03 02:02:58 -0800

    Use a cached version for the homepage for speed.

--- main_view.py
+++ main_view.py
@@ -275,7 +275,7 @@ def WriteTemplate(request, response, template_name, params, mime_type='text/html
   response.out.write(template.render(path, params))
 
 
-urls_to_handlers = [('/', MainPage),
+urls_to_handlers = [('/home', MainPage),  # Use this to generate templates/home_cached.html
                     ('/about', AboutPage),
                     ('/buynow', BuyNowExperiment),
                     ('/done_with_puzzle', DoneWithPuzzle),
